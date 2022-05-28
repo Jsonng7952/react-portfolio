64407c5aeea717515fda396c092ae905b61e228e
Jason <jason.ng7952@gmail.com>
2022-05-28 16:54:33 -0400

    Removed background color and position

--- src/components/styles/App.styled.js
+++ src/components/styles/App.styled.js
@@ -3,8 +3,6 @@ import styled from "styled-components";
 export const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
-  position: relative;
-  background-color: ${props => props.theme.primaryBody};
   height: 100vh;
   overflow: hidden;
 `
