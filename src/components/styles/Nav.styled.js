c270a655f56707ab28acef42dcf0b1dd0cf56e83
Jason <jason.ng7952@gmail.com>
2022-05-28 16:54:58 -0400

    Added brackets to the nav links

--- src/components/styles/Nav.styled.js
+++ src/components/styles/Nav.styled.js
@@ -12,7 +12,6 @@ export const NavContainer = styled.div`
 export const LinkContainer = styled.ul`
   display: flex;
   gap: 1rem;
-  border-bottom: 2px solid ${props => props.theme.primaryText};
   line-height: 1.6;
 `
 
@@ -25,5 +24,18 @@ export const LogoLink = styled.h1`
 export const TextLink = styled.li`
   list-style: none;
   font-size: 0.8rem;
+  padding: 0 1rem;
   color: ${props => props.theme.primaryText};
+
+  background-image: linear-gradient(${props => props.theme.primaryText}, ${props => props.theme.primaryText}),
+                    linear-gradient(${props => props.theme.primaryText}, ${props => props.theme.primaryText}),
+                    linear-gradient(${props => props.theme.primaryText}, ${props => props.theme.primaryText}),
+                    linear-gradient(${props => props.theme.primaryText}, ${props => props.theme.primaryText});
+
+  background-repeat: no-repeat;
+  background-size: 8px 1px;
+  background-position: top left, top right, bottom left, bottom right;
+
+  border: solid ${props => props.theme.primaryText};
+  border-width: 0 1px;
 `
