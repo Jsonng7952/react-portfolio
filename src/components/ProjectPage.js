572e5373e39bfac0b344c3bfd31712430270c43d
Jason <jason.ng7952@gmail.com>
2022-05-28 16:52:21 -0400

    Added fill-in projects

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -9,7 +9,7 @@ import {
   ProjectDescription, 
   ProjectLink, 
   ProjectSplit } from './styles/ProjectPage.styled';
-  import FortniteShop from '../assets/images/fortnite-shop.png'
+import FortniteShop from '../assets/images/fortnite-shop.png'
 
 
 function ProjectPage() {
@@ -29,8 +29,62 @@ function ProjectPage() {
               <p>Built using React, React-Router and Fortnite API.</p>
             </ProjectDescription>            
           </ProjectSplit>
-          <ProjectLink>Links</ProjectLink>
-        </ProjectCard>          
+          <ProjectLink>
+            <a href="https://jsonng7952.github.io/fortnite-shopping-cart/">Live Demo</a>
+            <a href="https://github.com/Jsonng7952/fortnite-shopping-cart">Repository</a>
+          </ProjectLink>
+        </ProjectCard>       
+        {/*Project 2*/}
+        <ProjectCard>
+          <ProjectTitle>Project 2</ProjectTitle>
+          <ProjectSplit>
+            <ProjectImage>
+              <img src={FortniteShop} alt='Fortnite Shop'></img>
+            </ProjectImage>
+            <ProjectDescription>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lobortis dolor odio, sit amet hendrerit dolor maximus quis. Suspendisse feugiat eu ex tempus consectetur.</p>  
+              <p>Built using Duis sagittis ipsum felis, ut mollis purus congue eget.</p>
+            </ProjectDescription>            
+          </ProjectSplit>
+          <ProjectLink>
+            <a href="">Live Demo</a>
+            <a href="">Repository</a>
+          </ProjectLink>
+        </ProjectCard>      
+        {/*Project 3*/}
+        <ProjectCard>
+          <ProjectTitle>Project 3</ProjectTitle>
+          <ProjectSplit>
+            <ProjectImage>
+              <img src={FortniteShop} alt='Fortnite Shop'></img>
+            </ProjectImage>
+            <ProjectDescription>
+              <p>Quisque porttitor mattis dui a ornare. Nulla vel dapibus leo, quis eleifend ligula. Proin imperdiet efficitur diam, eget imperdiet mauris vestibulum a. Vivamus pulvinar nisi eros, ac dictum eros porta ac. Phasellus fringilla tellus est, sed hendrerit nunc rutrum vel. Sed cursus lectus vel justo ullamcorper, eu dictum libero placerat.</p>  
+              <p>Built using Duis sagittis ipsum felis, ut mollis purus congue eget.</p>
+            </ProjectDescription>            
+          </ProjectSplit>
+          <ProjectLink>
+            <a href="">Live Demo</a>
+            <a href="">Repository</a>
+          </ProjectLink>
+        </ProjectCard>
+        {/*Project 4*/}
+        <ProjectCard>
+          <ProjectTitle>Project 4</ProjectTitle>
+          <ProjectSplit>
+            <ProjectImage>
+              <img src={FortniteShop} alt='Fortnite Shop'></img>
+            </ProjectImage>
+            <ProjectDescription>
+              <p>Quisque porttitor mattis dui a ornare. Nulla vel dapibus leo, quis eleifend ligula. Proin imperdiet efficitur diam, eget imperdiet mauris vestibulum a.</p>  
+              <p>Built using Duis sagittis ipsum felis, ut mollis purus congue eget.</p>
+            </ProjectDescription>            
+          </ProjectSplit>
+          <ProjectLink>
+            <a href="">Live Demo</a>
+            <a href="">Repository</a>
+          </ProjectLink>
+        </ProjectCard>                
       </ProjectContent>
     </ProjectContainer>
   )
