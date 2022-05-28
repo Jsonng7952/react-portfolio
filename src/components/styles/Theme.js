a672421038ef2145d9ea9691f362742e27fedd89
Jason <jason.ng7952@gmail.com>
2022-05-28 16:53:50 -0400

    Changed name for inverse color

--- src/components/styles/Theme.js
+++ src/components/styles/Theme.js
@@ -1,15 +1,15 @@
 export const LightTheme = {
   primaryBody: "#FFFFFF",
   primaryText: "#000000",
-  secondBody: "#000000",
-  secondText: "#FFFFFF",
+  inverseBody: "#000000",
+  inverseText: "#FFFFFF",
   fontFamily: "'Source Sans Pro', sans-serif",
 }
 
 export const DarkTheme = {
   primaryBody: "#000000",
   primaryText: "#FFFFFF",
-  secondBody: "#FFFFFF",
-  secondText: "#000000",
+  inverseBody: "#FFFFFF",
+  inverseText: "#000000",
   fontFamily: "'Source Sans Pro', sans-serif",
 }
