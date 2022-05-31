09e64eb11e88ead9ef1ee2c1c098b0a1646d2731
Jason <jason.ng7952@gmail.com>
2022-05-31 16:54:18 -0400

    Added initial layout for about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import { AboutContainer } from './styles/AboutPage.styled';
+import { AboutContainer, AboutContent, AboutSection, AboutText, AboutSkills, SkillContainer, TechIcon, TechName } from './styles/AboutPage.styled';
 import { StyledHeader } from './styles/PageStyles';
+import { JavaScriptLogo, HTMLLogo, CSSLogo, ReactLogo, NodeJSLogo, GITLogo } from '../assets/Svgs';
+import ProfileFull from '../assets/images/profile-full.png'
 
 function AboutPage() {
   return (
     <AboutContainer>
       <StyledHeader>ABOUT</StyledHeader>
+      <AboutContent>
+        <AboutSection>
+          <div>
+            <AboutText>
+              <p>I'm a front-end developer located in New York. I love to create simple yet beautiful websites with great user experience.</p>
+              <p>I work with React, JavaScript (ES6+), CSS and HTML to deliver quality front-end products.</p>
+              <p>I'm plan on working with NodeJS and EXPRESS for the back-end.</p>
+            </AboutText> 
+            <AboutSkills>
+              <SkillContainer>
+                <TechIcon>
+                  <JavaScriptLogo />             
+                  <TechName>JAVASCRIPT</TechName>     
+                </TechIcon>
+                <TechIcon>
+                  <HTMLLogo />             
+                  <TechName>HTML</TechName>     
+                </TechIcon>
+                <TechIcon>
+                  <CSSLogo />             
+                  <TechName>CSS</TechName>     
+                </TechIcon>
+                <TechIcon>
+                  <ReactLogo />             
+                  <TechName>REACT</TechName>     
+                </TechIcon>
+                <TechIcon>
+                  <NodeJSLogo />             
+                  <TechName>NODE JS</TechName>     
+                </TechIcon>
+                <TechIcon>
+                  <GITLogo />             
+                  <TechName>GIT</TechName>     
+                </TechIcon>               
+              </SkillContainer>
+            </AboutSkills>              
+          </div>
+          <img src={ProfileFull} alt=''></img>   
+        </AboutSection>
+      </AboutContent>
     </AboutContainer>
   )
 }
--- src/components/styles/AboutPage.styled.js
+++ src/components/styles/AboutPage.styled.js
@@ -2,5 +2,75 @@ import styled from "styled-components";
 import { StyledContainer } from "./PageStyles";
 
 export const AboutContainer = styled(StyledContainer)`
+  font-family: ${props => props.theme.fontFamily};
+  flex-grow: 1;
+`
 
+export const AboutContent = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const AboutSection = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 300px;
+  align-items: center;
+  column-gap: 2em;
+  padding: 0 2em;
+  color: ${props => props.theme.primaryText};
+  max-width: 2000px;
+
+  p:not(:last-child) {
+    margin-bottom: 0.5em;
+  }
+
+  img {
+    position: relative;
+    grid-row: -1;
+    grid-column: 2 / 3;
+    max-width: 100%;
+    height: auto;
+  }
+
+  // Temporary
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    img {
+      max-width: 300px;
+      max-height: 300px;
+    }
+  }
+`
+
+export const AboutText = styled.div`
+  font-size: clamp(0.8rem, 1.5vw, 2rem);
+`
+
+export const AboutSkills = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+`
+
+export const SkillContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(60px, 150px));
+`
+
+export const TechIcon = styled.div`
+  flex: 1;
+  margin: 1em;
+
+  svg {
+    width: 100%;
+    height: auto;
+  }
+`
+
+export const TechName = styled.div`
+  text-align: center;
+  font-size: 0.8em;
 `
