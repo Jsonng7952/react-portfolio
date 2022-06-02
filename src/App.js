760e7e96e061c12e24c78b00309b0b380d04f6dc
Jason <jason.ng7952@gmail.com>
2022-06-01 20:07:53 -0400

    Remove contact page link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import ProjectPage from './components/ProjectPage';
-import ContactPage from './components/ContactPage';
 import { GlobalStyle } from './components/styles/Global';
 import { ThemeProvider } from 'styled-components';
 import { DarkTheme, LightTheme } from './components/styles/Theme'
@@ -31,7 +30,6 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/about" element={<AboutPage />} />
               <Route path="/projects" element={<ProjectPage />} />
-              <Route path="/contact" element={<ContactPage />} />
             </Routes>            
           </AppContainer>
         </Router>     
