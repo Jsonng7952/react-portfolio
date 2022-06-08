da12166033cd1ebf98a1e9dfaf6218caf804a129
Jason <jason.ng7952@gmail.com>
2022-06-08 13:59:31 -0400

    Added basename for fresh page loading

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Router>
+        <Router basename="/react-portfolio">
           <AppContainer>
             <Nav themeChange={themeChange}/>
             <Routes>  
